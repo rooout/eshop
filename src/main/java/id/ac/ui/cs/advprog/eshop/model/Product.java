493db1c9ff2d239f9597e3a9f902d16588239d4f
NAME <EMAIL>
2025-02-12 16:55:28 +0700

    add delete feature

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -4,20 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.UUID;
-
+import java.util.UUID;
 @Getter @Setter
 public class Product {
-    private String productId;
+    private String productId = UUID.randomUUID().toString();
     private String productName;
     private int productQuantity;
-
-    public Product() {
-        this.productId = UUID.randomUUID().toString();
-    }
-
-    public Product(String productName, int productQuantity) {
-        this.productId = UUID.randomUUID().toString();
-        this.productName = productName;
-        this.productQuantity = productQuantity;
-    }
 }
--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -1,11 +1,17 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
+
 import java.util.List;
 
 public interface ProductService {
     public Product create(Product product);
+
     public List<Product> findAll();
+
+    public Product findById(String id);
+
     public Product update(Product product);
 
+    public void delete(String id);
 }
