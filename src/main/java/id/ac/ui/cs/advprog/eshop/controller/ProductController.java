5c62e74ecae1af67b0c7bf1c97711b403bc705ee
NAME <EMAIL>
2025-02-13 17:25:54 +0700

    fix edit product

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -1,45 +1,63 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
-import id.ac.ui.cs.advprog.eshop.model.Product;
-import id.ac.ui.cs.advprog.eshop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.ProductService;
+
 
 @Controller
 @RequestMapping("/product")
 public class ProductController {
-
     @Autowired
-    private ProductService service;
+    private ProductService productService;
+
+    @GetMapping("/")
+    public String getHome(Model model) {
+        return "index";
+    }
 
     @GetMapping("/create")
-    public String createProductPage(Model model) {
+    public String createProduct(Model model) {
         Product product = new Product();
         model.addAttribute("product", product);
         return "createProduct";
     }
 
     @PostMapping("/create")
-    public String createProductPost(@ModelAttribute Product product, Model model) {
-        service.create(product);
+    public String createProduct(@ModelAttribute Product product, Model model) {
+        productService.create(product);
         return "redirect:list";
     }
 
     @GetMapping("/list")
-    public String productListPage(Model model) {
-        List<Product> allProducts = service.findAll();
-        model.addAttribute("products", allProducts);
+    public String listProduct(Model model) {
+        model.addAttribute("products", productService.findAll());
         return "productList";
     }
 
-    @PostMapping("/update")
-    public String updateProduct(@ModelAttribute Product product) {
-        service.update(product);
-        return "redirect:list";
+    @GetMapping("/update/{id}")
+    public String updateProduct(@PathVariable("id") String id, Model model) {
+        Product product = productService.findById(id);
+        if (product == null) {
+            return "redirect:/product/list"; // Jika produk tidak ditemukan, kembali ke daftar
+        }
+        model.addAttribute("product", product);
+        return "editProduct";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateProduct(@PathVariable("id") String id, @ModelAttribute Product product, Model model) {
+        product.setProductId(id);
+        productService.update(product);
+        return "redirect:/product/list";
     }
 
 }
