bc185392d46bf5f8979bb211a9ed9b74ec4c43ea
NAME <EMAIL>
2025-03-07 12:42:23 +0700

    fix car service

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarService.java
@@ -1,17 +1,11 @@
 package id.ac.ui.cs.advprog.eshop.service;
-
-import java.util.List;
-
 import id.ac.ui.cs.advprog.eshop.model.Car;
+import java.util.List;
 
 public interface CarService {
     public Car create(Car car);
-
-    public List<Car> findAll();
-
+    public List <Car> findAll();
     Car findById(String carId);
-
     public void update(String carId, Car car);
-
     public void deleteCarById(String carId);
 }
