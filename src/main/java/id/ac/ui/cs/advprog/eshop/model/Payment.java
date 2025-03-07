4a04b4080cf0e5d87104eba97289f155c6ed2627
NAME <EMAIL>
2025-03-07 14:55:24 +0700

    [REFACTOR] Apply PaymentMethod and PaymentStatus to Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,7 +1,8 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Getter;
-
 import java.util.Map;
 import java.util.UUID;
 import java.util.regex.Matcher;
